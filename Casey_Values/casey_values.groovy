22c1470eaac8ba1e3b30ab1cb164d94c016aab16
Pete Mariani <pcmariani@gmail.com>
2024-01-17 15:10:14 -0500

    initial

---
+++ Casey_Values/casey_values.groovy
@@ -0,0 +1,22 @@
+import java.util.Properties;
+import java.io.InputStream;
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput;
+import com.boomi.execution.ExecutionUtil;
+
+logger = ExecutionUtil.getBaseLogger()
+
+for( int i = 0; i < dataContext.getDataCount(); i++ ) {
+    InputStream is = dataContext.getStream(i);
+    Properties props = dataContext.getProperties(i);
+
+    def root = new JsonSlurper().parse(is)
+    root.params.each { 
+      it.value_list_string = it.value_list.collect{ it.value }.join("---") 
+    } 
+
+    is = new ByteArrayInputStream(prettyJson(root).getBytes("UTF-8"));
+    dataContext.storeStream(is, props);
+}
+
+private static String prettyJson(def thing) { return JsonOutput.prettyPrint(JsonOutput.toJson(thing)) }
