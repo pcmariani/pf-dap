5d9ff8d312d2a73a591f0cf2057fe9064e74de60
Pete Mariani <pcmariani@gmail.com>
2024-01-22 10:33:24 -0500

    initial commit

---
+++ pivotConfigsJsonFlowTable/pivotConfigsJsonFlowTable.groovy
@@ -0,0 +1,23 @@
+import java.util.Properties;
+import java.io.InputStream;
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput;
+import com.boomi.execution.ExecutionUtil;
+
+logger = ExecutionUtil.getBaseLogger()
+
+for( int i = 0; i < dataContext.getDataCount(); i++ ) {
+    InputStream is = dataContext.getStream(i);
+    Properties props = dataContext.getProperties(i);
+
+    def root = new JsonSlurper().parse(is)
+
+    def outdata = prettyJson(root)
+       
+    is = new ByteArrayInputStream(outData.getBytes("UTF-8"));
+    dataContext.storeStream(is, props);
+}
+
+private static String prettyJson(def thing) { 
+    return JsonOutput.prettyPrint(JsonOutput.toJson(thing))
+}
