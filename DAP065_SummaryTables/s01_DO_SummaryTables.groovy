f8dd1a44684cc3177a174232992a41ea892abdb0
Pete Mariani <pcmariani@gmail.com>
2024-02-05 11:06:54 -0500

    changes

--- DAP065_SummaryTables/s01_DO_SummaryTables.groovy
+++ DAP065_SummaryTables/s01_DO_SummaryTables.groovy
@@ -36,26 +36,10 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     def sqlParamUserInputValuesJson = props.getProperty("document.dynamic.userdefined.ddp_sqlParamUserInputValuesJson")
     def sqlParamUserInputValues = sqlParamUserInputValuesJson ? new JsonSlurper().parseText(sqlParamUserInputValuesJson) : []
     // println prettyJson(sqlParamUserInputValues)
-    def pivotedDataConfigsJson = props.getProperty("document.dynamic.userdefined.ddp_PivotedDataConfigsConsolidated")
-    def pivotedDataConfigsArr = pivotedDataConfigsJson ? new JsonSlurper().parseText(pivotedDataConfigsJson)?.Records : []
-    // println prettyJson(pivotedDataConfigsArr)
-    // println pivotedDataConfigsArr.size()
-    def groupByConfigsJson = props.getProperty("document.dynamic.userdefined.ddp_GroupByConfigsConsolidated")
-    def activeGroupByConfigsArr = groupByConfigsJson ? new JsonSlurper().parseText(groupByConfigsJson)?.Records : []
-    // println prettyJson(activeGroupByConfigsArr)
-    // println activeGroupByConfigsArr.size()
-    def sourceJson = props.getProperty("document.dynamic.userdefined.ddp_Sources")
-    def source = new JsonSlurper().parseText(sourceJson).Records
-    // println prettyJson(source)
-
 
 
     ArrayList sortKeysArr = sqlParamUserInputValues.findAll { it.isSorted }.Value[0].split(/\s*,\s*/)
     // println sortKeysArr
-
-    def tableDefinitionJson = props.getProperty("document.dynamic.userdefined.ddp_TableDefinition")
-    def tableDefinition = new JsonSlurper().parseText(tableDefinitionJson).Records[0]
-    // println tableDefinition
 
     // Virtual Columns
     // println sqlColumnNamesArr
@@ -133,68 +117,6 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     }
 
     props.setProperty("document.dynamic.userdefined.ddp_sqlColumnNames", sqlColumnNamesArr.join(OFS))
-
-
-
-    //
-    // /* --- construct table title --- */
-    //
-    // // set tableTitleText
-    // def tableTitleText = "Table Title Text Not Yet Configured"
-    // if (source.ResultTableType =~ /(?i)Summary/){
-    //   tableTitleText = sectionNumber + "-1. " +
-    //   ( tableDefinition.TableTitleText_Summary != null && tableDefinition.TableTitleText_Summary != ""
-    //   ? tableDefinition.TableTitleText_Summary
-    //   : tableTitleText )
-    // }
-    // else if (source.ResultTableType =~ /(?i)Data/) {
-    //   if (tableInstanceRoot.TableTitleOverride) {
-    //     tableTitleText = sectionNumber + "-" + tableInstanceIndex.toString() + ". " +
-    //     ( tableDefinition.TableTitleOverride != null && tableDefinition.TableTitleOverride != ""
-    //     ? tableDefinition.TableTitleOverride
-    //     : tableTitleText )
-    //   }
-    //   else {
-    //     println tableTitleText
-    //     println tableDefinition.TableTitleText
-    //     tableTitleText = sectionNumber + "-" + tableInstanceIndex.toString() + ". " +
-    //     ( tableDefinition.TableTitleText != null && tableDefinition.TableTitleText != ""
-    //     ? tableDefinition.TableTitleText
-    //     : tableTitleText )
-    //   }
-    // }
-    // // println tableTitleText
-    //
-    // // for tableTitleText, replace placeholders with values, apply stringReplacements
-    // def stringReplacementsArr = tableDefinition.TableTitleStringReplacements
-    // // println stringReplacementsArr
-    // (tableTitleText =~ /\{\{(.*?)\}\}/).collect{match -> match[1]}.unique().each() { name ->
-    //   def value = valuesMap[name]
-    //   // println name
-    //   if (!value) {
-    //     def propName = name.replaceAll(" ","_")
-    //     value = ExecutionUtil.getDynamicProcessProperty("DPP_" + propName)
-    //     if (!value) value = ExecutionUtil.getDynamicProcessProperty(propName)
-    //     else if (!value) value = props.getProperty("document.dynamic.userdefined.ddp_" + propName)
-    //     else if (!value) value = props.getProperty("document.dynamic.userdefined." + propName)
-    //   }
-    //   // println value
-    //   if (value) {
-    //     def replacementsArr = stringReplacementsArr.findAll{ name == it.PlaceHolder.replaceAll(/[\{\{\}\}]/,"") }
-    //     replacementsArr.each { replacement ->
-    //       value = value.replaceAll(replacement.SearchText, replacement.ReplaceText)
-    //     }
-    //     tableTitleText = tableTitleText.replaceAll(/\{\{$name\}\}/, value)
-    //   }
-    // }
-    // tableTitleText = tableTitleText.replaceAll("<", "&lt;").replaceAll(">", "&gt;")
-    // // println tableTitleText
-    //
-
-
-
-
-
 
 
     is = new ByteArrayInputStream(outData.toString().getBytes("UTF-8"));
--- DAP065_tableTitleText/s01_CONSTRUCT_TableTitles.groovy
+++ DAP065_tableTitleText/s01_CONSTRUCT_TableTitles.groovy
@@ -18,56 +18,30 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     InputStream is = dataContext.getStream(i);
     Properties props = dataContext.getProperties(i);
 
-    // int reportContentItemId = (props.getProperty("document.dynamic.userdefined.ddp_ReportContentItem_DynamicTableId") ?: "1") as int
-    // int tableDefinitionId = (props.getProperty("document.dynamic.userdefined.ddp_TableDefinitionId") ?: "1") as int
-    def sqlParamValues = props.getProperty("document.dynamic.userdefined.ddp_sqlParamValues")
-    // println sqlParamValues
-    def sqlColumnNames = props.getProperty("document.dynamic.userdefined.ddp_sqlColumnNames")
-    // println sqlColumnNames
-    def sqlColumnNamesArr = sqlColumnNames.split(IFS) as ArrayList
-    // println sqlColumnNamesArr
-    int tableInstanceIndex = (props.getProperty("document.dynamic.userdefined.ddp_tableInstanceIndex") ?: "1") as int
-    // println tableInstanceIndex
-    int tableInstanceId = props.getProperty("document.dynamic.userdefined.ddp_TableInstanceId") as int
-    // println tableInstanceId
-    def virtualColumnsJson = props.getProperty("document.dynamic.userdefined.ddp_VirtualColumns")
-    def virtualColumns = virtualColumnsJson ? new JsonSlurper().parseText(virtualColumnsJson).Records : []
-    // println prettyJson(virtualColumns)
+    // INPUTS //
+
+    def resultTableType = props.getProperty("document.dynamic.userdefined.ddp_ResultTableType")
+
     def sqlParamUserInputValuesJson = props.getProperty("document.dynamic.userdefined.ddp_sqlParamUserInputValuesJson")
     def sqlParamUserInputValues = sqlParamUserInputValuesJson ? new JsonSlurper().parseText(sqlParamUserInputValuesJson) : []
-    // println prettyJson(sqlParamUserInputValues)
-    def pivotedDataConfigsJson = props.getProperty("document.dynamic.userdefined.ddp_PivotedDataConfigsConsolidated")
-    def pivotedDataConfigsArr = pivotedDataConfigsJson ? new JsonSlurper().parseText(pivotedDataConfigsJson)?.Records : []
-    // println prettyJson(pivotedDataConfigsArr)
-    // println pivotedDataConfigsArr.size()
-    def groupByConfigsJson = props.getProperty("document.dynamic.userdefined.ddp_GroupByConfigsConsolidated")
-    def activeGroupByConfigsArr = groupByConfigsJson ? new JsonSlurper().parseText(groupByConfigsJson)?.Records : []
-    // println prettyJson(activeGroupByConfigsArr)
-    // println activeGroupByConfigsArr.size()
-    def sourceJson = props.getProperty("document.dynamic.userdefined.ddp_Sources")
-    def source = new JsonSlurper().parseText(sourceJson).Records
-    // println prettyJson(source)
-
-
-    ArrayList sortKeysArr = sqlParamUserInputValues.findAll { it.isSorted }.Value[0].split(/\s*,\s*/)
-    // println sortKeysArr
 
     def tableDefinitionJson = props.getProperty("document.dynamic.userdefined.ddp_TableDefinition")
     def tableDefinition = new JsonSlurper().parseText(tableDefinitionJson).Records[0]
     // println tableDefinition
 
 
-
-    /* --- construct table title --- */
+    // LOGIC //
 
     // set tableTitleText
     def tableTitleText = "Table Title Text Not Yet Configured"
-    if (source.ResultTableType =~ /(?i)Summary/){
+
+    if (resultTableType =~ /(?i)Summary/){
       tableTitleText = sectionNumber + "-1. " +
       ( tableDefinition.TableTitleText_Summary != null && tableDefinition.TableTitleText_Summary != ""
       ? tableDefinition.TableTitleText_Summary
       : tableTitleText )
     }
+
     else if (source.ResultTableType =~ /(?i)Data/) {
       if (tableInstanceRoot.TableTitleOverride) {
         tableTitleText = sectionNumber + "-" + tableInstanceIndex.toString() + ". " +
@@ -76,8 +50,6 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
         : tableTitleText )
       }
       else {
-        println tableTitleText
-        println tableDefinition.TableTitleText
         tableTitleText = sectionNumber + "-" + tableInstanceIndex.toString() + ". " +
         ( tableDefinition.TableTitleText != null && tableDefinition.TableTitleText != ""
         ? tableDefinition.TableTitleText
@@ -89,17 +61,18 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     // for tableTitleText, replace placeholders with values, apply stringReplacements
     def stringReplacementsArr = tableDefinition.TableTitleStringReplacements
     // println stringReplacementsArr
+
     (tableTitleText =~ /\{\{(.*?)\}\}/).collect{match -> match[1]}.unique().each() { name ->
-      def value = valuesMap[name]
       // println name
-      if (!value) {
+      def value = sqlParamUserInputValues?.find{it.UserInputName == name}?.Value
+      // println value
+      if (!value || value == null) {
         def propName = name.replaceAll(" ","_")
         value = ExecutionUtil.getDynamicProcessProperty("DPP_" + propName)
         if (!value) value = ExecutionUtil.getDynamicProcessProperty(propName)
         else if (!value) value = props.getProperty("document.dynamic.userdefined.ddp_" + propName)
         else if (!value) value = props.getProperty("document.dynamic.userdefined." + propName)
       }
-      // println value
       if (value) {
         def replacementsArr = stringReplacementsArr.findAll{ name == it.PlaceHolder.replaceAll(/[\{\{\}\}]/,"") }
         replacementsArr.each { replacement ->
@@ -111,16 +84,9 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     tableTitleText = tableTitleText.replaceAll("<", "&lt;").replaceAll(">", "&gt;")
     // println tableTitleText
 
-
-
-
-
-
-
-
     // is = new ByteArrayInputStream(outData.toString().getBytes("UTF-8"));
     dataContext.storeStream(is, props);
 
 }
 
-private static String prettyJson(def thing) { return JsonOutput.prettyPrint(JsonOutput.toJson(thing)) }
+// private static String prettyJson(def thing) { return JsonOutput.prettyPrint(JsonOutput.toJson(thing)) }
