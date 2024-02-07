78fc72f9dc10ddc369873377418bef0d67cb3cdf
Pete Mariani <pcmariani@gmail.com>
2024-02-07 11:34:11 -0500

    now used only active keys for param values

--- DAP065_SortMultiselect/s01_SORT_MultiSelectParam.groovy
+++ DAP065_SortMultiselect/s01_SORT_MultiSelectParam.groovy
@@ -28,30 +28,35 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
 
     if (multiSelectParam) {
       def param = multiSelectParam[0]
-      def pivotKeysArr = []
+      ArrayList pivotKeysArr = []
+      ArrayList activePivotKeysArrsArr = []
 
       if (param.PivotConfig == "GroupBy") {
         def groupByConfigsJson = props.getProperty("document.dynamic.userdefined.ddp_GroupByConfigsConsolidated")
         def groupByConfigsArr = groupByConfigsJson ? new JsonSlurper().parseText(groupByConfigsJson)?.Records : []
         // println prettyJson(groupByConfigsArr)
-        pivotKeysArr = groupByConfigsArr.RowKey.collect { it.toUpperCase().split(DBIFS) }.transpose()
+        activePivotKeysArrsArr = groupByConfigsArr.findAll{it.Active == true}.RowKey.collect { it.toUpperCase().split(DBIFS) }.transpose()
       }
       else if (param.PivotConfig == "PivotOn") {
         def pivotedDataConfigsJson = props.getProperty("document.dynamic.userdefined.ddp_PivotedDataConfigsConsolidated")
         def pivotedDataConfigsArr = pivotedDataConfigsJson ? new JsonSlurper().parseText(pivotedDataConfigsJson)?.Records : []
         // println prettyJson(pivotedDataConfigsArr)
-        pivotKeysArr = pivotedDataConfigsArr.ColumnKey.collect { it.toUpperCase().split(DBIFS) }.transpose()
+        activePivotKeysArrsArr = pivotedDataConfigsArr.findAll{it.Active == true}.ColumnKey.collect { it.toUpperCase().split(DBIFS) }.transpose()
       }
+      // println activePivotKeysArrsArr
 
-      if (pivotKeysArr) {
-        pivotKeysArr.each {
+      if (activePivotKeysArrsArr) {
+        activePivotKeysArrsArr.each { activePivotKeysArr ->
           ArrayList paramValuesArr = multiSelectParam.Value[0].toUpperCase().split(/\s*,\s*/)
-          // if (!paramValuesArr.disjoint(it)) {                                //  NO - if one param value exists in pivotKeysArr
-          // if (paramValuesArr.clone().sort() == it.clone().sort()) {          //  NO - if param values and pivotKeysArr are all the same (not in same order)
-          if (paramValuesArr.intersect(it).size() == paramValuesArr.size()) {   // YES - if all param values exist in pivotKeysArr (but pivotKeysArr could have more values)
-            sortedParamValuesArr = paramValuesArr.sort{ m -> it.indexOf(m) } 
+          // if (!paramValuesArr.disjoint(it)) {                                  // NO - if one param value exists in pivotKeysArr
+          // if (paramValuesArr.clone().sort() == it.clone().sort()) {            // NO - if param values and pivotKeysArr are all the same (not in same order)
+          // if (paramValuesArr.intersect(it).size() == paramValuesArr.size()) {  // NO - if all param values exist in pivotKeysArr (but pivotKeysArr could have more values)
+          def activePivotKeysAlsoInParamValuesArr = activePivotKeysArr.intersect(paramValuesArr)
+          if (activePivotKeysAlsoInParamValuesArr.size() == activePivotKeysArr.size()) {                 // YES - if all pivot keys exist in paramValuesArr (but paramValuesArr could have more values)
+            // sortedParamValuesArr = paramValuesArr.sort{ m -> activePivotKeysArr.indexOf(m) } 
             // println sortedParamValuesArr
-            param.Value = sortedParamValuesArr.join(", ")
+            // param.Value = sortedParamValuesArr.join(", ")
+            param.Value = activePivotKeysAlsoInParamValuesArr.join(", ")
             param.isSorted = true
           }
         }
