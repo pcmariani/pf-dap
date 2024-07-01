c86e646c000c9576cfd4332c252236a67e355b37
Pete Mariani <pcmariani@gmail.com>
2024-07-01 11:21:21 -0400

    now surrounds tablegroups with root tag to handle multiple tablegroups

--- formatting/s10_FORMAT_TableTitles.groovy
+++ formatting/s10_FORMAT_TableTitles.groovy
@@ -5,9 +5,7 @@ import com.boomi.execution.ExecutionUtil;
 
 logger = ExecutionUtil.getBaseLogger()
 
-def sectionNumber = ExecutionUtil.getDynamicProcessProperty("DPP_SectionNumber") ?: "0.0.0.0.0"
-//println sectionNumber
-def tot_prefixOption = ExecutionUtil.getDynamicProcessProperty("dpp_figure_no_style") ?: "Prefix"
+def tot_prefixOption = ExecutionUtil.getDynamicProcessProperty("dpp_table_no_style") ?: "No Prefix"
 //println tot_prefixOption
 def tot_text_to_prepend = (ExecutionUtil.getDynamicProcessProperty("DPP_TableOfTables_TextToPrepend") ?: "Table" ) - ~/\s*$/
 //println tot_text_to_prepend
@@ -16,18 +14,19 @@ for( int i = 0; i < dataContext.getDataCount(); i++ ) {
     InputStream is = dataContext.getStream(i);
     Properties props = dataContext.getProperties(i);
 
-    def root = new XmlSlurper().parseText(is.text.replaceAll(/&/,"&amp;"))
+    def data = is.text.replaceAll(/&/,"&amp;")
+    def root = new XmlSlurper().parseText("<root>$data</root>")
 
     // if the table title contains the section prefix, remove it
-    def tableTitle = root.h3.toString().replaceAll(/^.*-\d{1,2}\.?\s*/,"")
+    def tableTitle = root.tablegroup.h3.toString().replaceAll(/^.*-\d{1,2}\.?\s*/,"")
 
     // if we want the section prefix, add it
     if (tot_prefixOption =~ /(?i)section prefix/) {
-        tableTitle = root.h3.@sectionPrefix.toString() + ". " + tableTitle
+        tableTitle = root.tablegroup.h3.@sectionPrefix.toString() + ". " + tableTitle
     }
     //println tableTitle
 
-    root.h3 = tableTitle
+    root.tablegroup.h3 = tableTitle
 
     String outData = XmlUtil.serialize(root) - ~/^.*\?>/
     is = new ByteArrayInputStream(outData.getBytes("UTF-8"));
